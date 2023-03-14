76273246fbdd88301265c184b5d4ef542c8bc0b6
Friedolin <friedolin.groeger@gmx.de>
2023-03-14 22:41:56 +0100

    Working mqtt client with homeassistant config

--- src/E3DC_display.cpp
+++ src/E3DC_display.cpp
@@ -23,16 +23,28 @@
 #include <WiFi.h>
 #include "esp_adc_cal.h"
 #include "time.h"
-#include "modbus.h"
+#include <M5_KMeter.h>
+#include <PubSubClient.h>
 
 const char* ssid             = "NETGEAR06";
 const char* password         = "fancyorchestra441";//lk13-cs63-eylg-rwao";
 const char* ntpServer        = "pool.ntp.org";
+const char* mqtt_server      = "homeassistant.fritz.box";
+const char* mqtt_id          = "ESPClient-temperatur";
+const char* mqtt_user        = "mqtt";
+const char* mqtt_password    = "mqtt";
+const char* mqtt_topic       = "homeassistant/sensor/boiler_temp/state";
 const long gmtOffset_sec     = 3600;
 const int daylightOffset_sec = 3600;
-
+const char* mqtt_config      = "{\"name\": \"boiler_temp\", \"device_class\": \"temperature\", \"unit_of_measurement\": \"°C\", \"state_topic\": \"homeassistant/sensor/boiler_temp/state\"}";
+const char* mqtt_config_topic = "homeassistant/sensor/boiler_temp/config";
 
 Ink_Sprite TimePageSprite(&M5.M5Ink);
+M5_KMeter sensor;
+WiFiClient espClient;
+PubSubClient client(espClient);
+int lastMeasurement = 0;
+
 
 void printLocalTimeAndSetRTC() {
     struct tm timeinfo;
@@ -132,24 +144,42 @@ void getNTPTime() {
     //WiFi.mode(WIFI_OFF);
 }
 
-void drawTimeAndDate(RTC_TimeTypeDef time, RTC_DateTypeDef date) {
+void mqtt_update() {
+    // Loop until we're reconnected
+    if (!client.connected()) {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect(mqtt_id, mqtt_user, mqtt_password)) {
+            Serial.println("connected");
+            // Once connected, publish an announcement...
+            client.publish(mqtt_config_topic, mqtt_config, true);
+            client.publish(mqtt_topic, String(sensor.getTemperature()).c_str());
+            // ... and resubscribe
+            //client.subscribe("inTopic");
+        } else {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+        }
+    }
+    client.loop();
+}
+
+void drawTimeAndDate(RTC_TimeTypeDef time, RTC_DateTypeDef date, float Temp, int e_temp, int e_wifi, int e_mqtt) {
     char buf[64];
 
-    snprintf(buf, 6, "%02d:%02d", time.Hours, time.Minutes);
-    TimePageSprite.drawString(40, 20, buf, &AsciiFont24x48);
+    snprintf(buf, 60, "%02d:%02d", time.Hours, time.Minutes);
+    TimePageSprite.drawString(4, 30, buf, &AsciiFont8x16);
     snprintf(buf, 60, "%02d.%02d.%02d", date.Date, date.Month,
              date.Year - 2000);
-    TimePageSprite.drawString(4, 70, buf, &AsciiFont24x48);
-    snprintf(buf, 60, "Solar:%6d W SG-R:%1d", solarPower, sgReadyReg);
-    TimePageSprite.drawString(4, 120, buf, &AsciiFont8x16);
-    snprintf(buf, 60, "Grid :%6d W", gridPower);
-    TimePageSprite.drawString(4, 140, buf, &AsciiFont8x16);
-    snprintf(buf, 60, "Akku :%6d W SOC:%3d%%", batPower, batSoc);
-    TimePageSprite.drawString(4, 160, buf, &AsciiFont8x16);
-    snprintf(buf, 60, "Haus :%6d W", homePower);
+    TimePageSprite.drawString(4, 50, buf, &AsciiFont8x16);
+    snprintf(buf, 60, "Temp:");
+    TimePageSprite.drawString(4, 80, buf, &AsciiFont24x48);
+    snprintf(buf, 60, "%.1f C", Temp);
+    TimePageSprite.drawString(4, 130, buf, &AsciiFont24x48);
+    sprintf(buf, "Lipo:%.2fV", getBatVoltage());
+    TimePageSprite.drawString(10, 2, buf, &AsciiFont8x16);
+    sprintf(buf, "temp:%d, wifi:%d, mqtt:%d", e_temp, e_wifi, e_mqtt);
     TimePageSprite.drawString(4, 180, buf, &AsciiFont8x16);
-    sprintf(buf,"Lipo:%.2fV",getBatVoltage());
-    TimePageSprite.drawString(10,2,buf,&AsciiFont8x16);
 }
 
 RTC_TimeTypeDef g_time;
@@ -161,6 +191,9 @@ void setup() {
     Wire1.begin(21, 22);
     uint8_t data = M5.rtc.ReadReg(0x01);
     M5.begin();
+    Wire.begin(SDA, SCL, 400000l);
+    sensor.begin();
+    sensor.setSleepTime(5);
 
     // Green LED - indicates ESP32 is running
     digitalWrite(LED_EXT_PIN, LOW);
@@ -172,12 +205,18 @@ void setup() {
 
     Serial.println("Power on by: power button");
     M5.M5Ink.clear();
-    // Fetch current time from Internet
-    getNTPTime();
-    initModbus(IP_E3DC);
-    firstReadMagicByte(mbIP_E3DC);
     TimePageSprite.creatSprite(0, 0, 200, 200);
     TimePageSprite.clear(CLEAR_DRAWBUFF | CLEAR_LASTBUFF);// );
+    // Fetch current time from Internet
+    getNTPTime();
+    M5.rtc.GetTime(&g_time);
+    M5.rtc.GetDate(&g_date);
+    sensor.update();
+    drawTimeAndDate(g_time, g_date, sensor.getTemperature(), sensor.getError(), WiFi.status(), client.connected());
+    TimePageSprite.pushSprite();
+    client.setServer(mqtt_server, 1883);
+    //client.setCallback(callback);
+
 }
 
 void loop() {
@@ -185,56 +224,57 @@ void loop() {
     Serial.println("loop");
     M5.rtc.GetTime(&g_time);
     M5.rtc.GetDate(&g_date);
+    mqtt_update();
+    
+    // Wait until full minute, also continue if full minute passed but still within first 10s
+    while (g_time.Seconds > 10) {
+        M5.rtc.GetTime(&g_time);
+        delay(200);
+        M5.update();      
+        if (M5.BtnPWR.wasPressed()) {
+            Serial.printf("Btn %d was pressed, shutdown\r\n", BUTTON_EXT_PIN);
+            Serial.flush();
+            M5.shutdown();
+        }
+        mqtt_update();
+    }
+    M5.rtc.GetDate(&g_date);
     // Full refresh once per hour
     if (g_time.Minutes % 10 == 0) {// == FULL_REFRESH_MINUTE - 1) {
         M5.M5Ink.clear();
         TimePageSprite.clear(CLEAR_DRAWBUFF | CLEAR_LASTBUFF);
     }
     Serial.flush();
-    mainMbRead();
-    checkMb();
-    if (countMbReset > 0) {
-      Serial.printf("countMbReset: %d", countMbReset);
-    }
-    if (countMbReboot > 0) {
-      Serial.printf("countMbReboot: %d", countMbReboot);
-    }
-
-    drawTimeAndDate(g_time, g_date);
-    TimePageSprite.pushSprite();
-
-
-    
-    // Wait until full minute, also continue if full minute passed but still within first 10s
-    while ((g_time.Seconds > 10)) {
-      M5.rtc.GetTime(&g_time);
-      delay(200);
-      M5.update();
-      if (M5.BtnPWR.wasPressed()) {
-        Serial.printf("Btn %d was pressed, shutdown\r\n", BUTTON_EXT_PIN);
-        Serial.flush();
-        M5.shutdown();
-      }
-    }
-    M5.rtc.GetDate(&g_date);
     //mainMbRead();
 
     // Draw new time and date
-    drawTimeAndDate(g_time, g_date);
+    sensor.update();
+    drawTimeAndDate(g_time, g_date, sensor.getTemperature(), sensor.getError(), WiFi.status(), client.connected());
     TimePageSprite.pushSprite();
-
+    if (client.connected()) {
+        client.publish(mqtt_topic, String(sensor.getTemperature()).c_str());
+        client.loop();
+        lastMeasurement = g_time.Seconds;
+    }
     checkBatteryVoltage(true);
 
     // Wait until 10s before full minute
-    while ((g_time.Seconds < 50)) {
-      M5.rtc.GetTime(&g_time);
-      delay(200);
-      M5.update();
-      if (M5.BtnPWR.wasPressed()) {
-        Serial.printf("Btn %d was pressed, shutdown\r\n", BUTTON_PWR_PIN);
-        Serial.flush();
-        M5.shutdown();
-      }
+    while (g_time.Seconds < 55) {
+        M5.rtc.GetTime(&g_time);
+        delay(200);
+        M5.update();
+        if (M5.BtnPWR.wasPressed()) {
+            Serial.printf("Btn %d was pressed, shutdown\r\n", BUTTON_PWR_PIN);
+            Serial.flush();
+            M5.shutdown();
+        }
+        mqtt_update();
+        if (client.connected() && g_time.Seconds >= lastMeasurement + 10) {
+            sensor.update();
+            client.publish(mqtt_topic, String(sensor.getTemperature()).c_str());
+            client.loop();
+            lastMeasurement = g_time.Seconds;
+        }
     }
     return;
 }
